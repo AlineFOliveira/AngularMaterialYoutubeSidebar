c7ce02d929b705780d185c0b1211c120248d6604
Aline <aliferroli18@gmail.com>
2024-05-05 17:03:21 -0300

    Sidebar se retraindo, rotas funcionando e content sendo mudado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { CustomSidenavComponent } from './custom-sidenav/custom-sidenav.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +16,15 @@ import { CustomSidenavComponent } from './custom-sidenav/custom-sidenav.componen
     MatButtonModule,
     MatIconModule,
     MatSidenavModule, 
-    CustomSidenavComponent
+    CustomSidenavComponent,
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angularYoutubeSidebar';
+  collapsed = signal(false);
+  sidenavWidth = computed(() => this.collapsed() ? '65px' : '250px');
 }
--- src/app/custom-sidenav/custom-sidenav.component.ts
+++ src/app/custom-sidenav/custom-sidenav.component.ts
@@ -1,23 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Input, computed, signal } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 
 export type MenuItem = {
   icon: string;
   label: string;
-  route: any;
+  route: string;
 }
 
 @Component({
   selector: 'app-custom-sidenav',
   standalone: true,
-  imports: [MatListModule],
+  imports: [MatListModule, MatIconModule, CommonModule, RouterModule],
   templateUrl: './custom-sidenav.component.html',
   styleUrl: './custom-sidenav.component.scss'
 })
 export class CustomSidenavComponent {
-  menuItems = signal<menuItem[]>([
+  sideNavCollapsed = signal(false);
+  @Input() set collapsed(val: boolean){
+    this.sideNavCollapsed.set(val);
+  }
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard',
+    },
+    {
+      icon: 'video_library',
+      label: 'Content',
+      route: 'content',
+    },
+    {
+      icon: 'analytics',
+      label: 'Analytics',
+      route: 'analytics',
+    },
+    {
+      icon: 'comment',
+      label: 'Comments',
+      route: 'comments',
+    }
+  ]);
 
-  ])
+  profilePicSize = computed(()=> this.sideNavCollapsed() ? '32' : '100');
 
 }
